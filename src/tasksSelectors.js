9da3fbd5f1e4c67c9c9dac050d7f897fa0159a33
NeanReal <doomfree64@gmail.com>
2026-02-24 16:58:47 +0800

    deleted bad prjct

--- src/tasksSelectors.js
+++
@@ -1,8 +0,0 @@
-export const selectAllTasks = (state) => state.tasks.items
-
-export const selectActiveTasks = (state) =>
-  state.tasks.items.filter((task) => !task.completed)
-
-export const selectCompletedTasks = (state) =>
-  state.tasks.items.filter((task) => task.completed)
-
